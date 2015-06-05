a8a12b2727ce6274d6e4566dcc176ffd60a1d673
Abel Sánchez Custodio <acsanchezcu@gmail.com>
2015-06-05 09:39:46 +0200

    Added project

---
+++ ASCViperArchitecture/ASCViperArchitecture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASCViperArchitecture
+//
+//  Created by Abel Sánchez Custodio on 5/6/15.
+//  Copyright (c) 2015 acsanchezcu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ASCViperArchitecture/ASCViperArchitecture/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ASCViperArchitecture
+//
+//  Created by Abel Sánchez Custodio on 5/6/15.
+//  Copyright (c) 2015 acsanchezcu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
